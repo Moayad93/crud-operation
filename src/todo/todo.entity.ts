1747bd308b428aaaa92e3d08b530c6707b2d3b55
moayad93 <moayad.alnu@gmail.com>
2021-07-02 15:34:19 +0300

    Configure the todo entity

---
+++ src/todo/todo.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('todos')
+export class TodoEntity {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column({ type: 'boolean', default: false, })
+  is_done: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
