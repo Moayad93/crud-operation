3dc371b54727e5ec4eb21cbb9d4d1cf7523022bb
moayad93 <moayad.alnu@gmail.com>
2021-07-02 16:25:07 +0300

    Add SwaggerModule and DocumentBuilder on main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('xxx')
+    .setDescription('api')
+    .setVersion('0.1')
+    .addTag('todo')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
