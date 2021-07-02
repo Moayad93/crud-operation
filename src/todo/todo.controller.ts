d606c8d9a4c13a5c2f05811ae338b5c8b5ed8c36
moayad93 <moayad.alnu@gmail.com>
2021-07-02 16:07:01 +0300

    Add CRUD configuration to todo.controller.ts file

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { Crud, CrudController } from '@nestjsx/crud';
+import { TodoEntity } from './todo.entity';
+import { TodoService } from './todo.service';
 
+@Crud({
+  model: {
+    type: TodoEntity,
+  },
+})
 @Controller('todo')
-export class TodoController {}
+export class TodoController implements CrudController<TodoEntity> {
+  constructor(public service: TodoService) {}
+}
